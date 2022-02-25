a3f6a4ac161fa38abeed79a11a1303e729bd91b4
facobackup <gustavomicaelbarbosa@gmail.com>
2022-02-25 13:00:40 -0300

    Multiplos ajustes

--- components/ControlBar.jsx
+++ components/ControlBar.jsx
@@ -55,9 +55,10 @@ export default function ControlBar(props) {
                     const promises = f.map(file => {
                         return new Promise(resolve => {
                             const folder = pathRequire.resolve(props.hook.path + '\\' + (props.hook.currentDirectory.id ? props.hook.currentDirectory.id : '') + '\\' + file.webkitRelativePath.replace(file.name, ''))
+                            console.log(folder)
                             if (!props.hook.fs.existsSync(folder))
                                 props.hook.fs.mkdir(folder, () => {
-                                    props.hook.fileSystem.importFile(file, folder, f)
+                                    props.hook.fileSystem.importFile(file, folder, false)
                                         .then(() => {
                                             resolve()
 
@@ -65,7 +66,7 @@ export default function ControlBar(props) {
                                 })
                             else
                                 props.hook.fileSystem
-                                    .importFile(file, folder, f)
+                                    .importFile(file, folder, false)
                                     .then(() => {
                                         resolve()
 
